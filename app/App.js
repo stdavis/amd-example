939d4e8102a3ec1f550a0c0c93aa8d058ce47f16
Scott Davis <stdavis@utah.gov>
2014-09-10 21:38:34 +0000

    Renamed sum module.

--- app/App.js
+++ app/App.js
@@ -1,10 +1,10 @@
-define(['app/Sum'], function (Sum) {
+define(['app/sum'], function (sum) {
     var a = document.getElementById('txtA');
     var b = document.getElementById('txtB');
     var btn = document.getElementById('btnSum');
     var result = document.getElementById('spanResult');
 
     btn.onclick = function () {
-        result.innerHTML = Sum(a.value, b.value);
-    }
+        result.innerHTML = sum(a.value, b.value);
+    };
 });
